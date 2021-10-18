b1c8d661030c3305c8f9018f496193b4ca72c553
richardingwe <richardingwe5@gmail.com>
2021-10-19 00:25:20 +0100

    added styling to invoice modal

--- src/components/InvoiceModal.vue
+++ src/components/InvoiceModal.vue
@@ -221,4 +221,142 @@
 	};
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+	.invoice-wrap {
+		position: fixed;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 100vh;
+		overflow: scroll;
+		&::-webkit-scrollbar {
+			display: none;
+		}
+		@media (min-width: 900px) {
+			left: 90px;
+		}
+		.invoice-content {
+			position: relative;
+			padding: 56px;
+			max-width: 700px;
+			width: 100%;
+			background-color: #141625;
+			color: #fff;
+			box-shadow: 10px 4px 6px -1px rgba(0, 0, 0, 0.2),
+				0 2px 4px -1px rgba(0, 0, 0, 0.06);
+			h1 {
+				margin-bottom: 48px;
+				color: #fff;
+			}
+			h3 {
+				margin-bottom: 16px;
+				font-size: 18px;
+				color: #777f98;
+			}
+			h4 {
+				color: #7c5dfa;
+				font-size: 12px;
+				margin-bottom: 24px;
+			}
+			// Bill To / Bill From
+			.bill-to,
+			.bill-from {
+				margin-bottom: 48px;
+				.location-details {
+					gap: 16px;
+					div {
+						flex: 1;
+					}
+				}
+			}
+			// Invoice Work
+			.invoice-work {
+				.payment {
+					gap: 24px;
+					div {
+						flex: 1;
+					}
+				}
+				.work-items {
+					.item-list {
+						width: 100%;
+						// Item Table Styling
+						.table-heading,
+						.table-items {
+							gap: 16px;
+							font-size: 12px;
+							.item-name {
+								flex-basis: 50%;
+							}
+							.qty {
+								flex-basis: 10%;
+							}
+							.price {
+								flex-basis: 20%;
+							}
+							.total {
+								flex-basis: 20%;
+								align-self: center;
+							}
+						}
+						.table-heading {
+							margin-bottom: 16px;
+							th {
+								text-align: left;
+							}
+						}
+						.table-items {
+							position: relative;
+							margin-bottom: 24px;
+							img {
+								position: absolute;
+								top: 15px;
+								right: 0;
+								width: 12px;
+								height: 16px;
+							}
+						}
+					}
+					.button {
+						color: #fff;
+						background-color: #252945;
+						align-items: center;
+						justify-content: center;
+						width: 100%;
+						img {
+							margin-right: 4px;
+						}
+					}
+				}
+			}
+			.save {
+				margin-top: 60px;
+				div {
+					flex: 1;
+				}
+				.right {
+					justify-content: flex-end;
+				}
+			}
+		}
+		.input {
+			margin-bottom: 24px;
+		}
+		label {
+			font-size: 12px;
+			margin-bottom: 6px;
+		}
+		input,
+		select {
+			width: 100%;
+			background-color: #1e2139;
+			color: #fff;
+			border-radius: 4px;
+			padding: 12px 4px;
+			border: none;
+			&:focus {
+				outline: none;
+			}
+		}
+	}
+</style>
