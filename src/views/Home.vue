d0352822c03998ca5127c4bce97e69f8796c70da
richardingwe <richardingwe5@gmail.com>
2021-10-18 19:17:00 +0100

    added the home component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,36 @@
 <template>
-  <div class="home"></div>
+	<div class="home container">
+		<!-- Header -->
+		<div class="header flex">
+			<div class="left flex flex-column">
+				<h1>Invoices</h1>
+				<span>There are 4 total invoices</span>
+			</div>
+			<div class="right flex">
+				<div @click="toggleFilterMenu" class="filter flex" ref="filter">
+					<span>Filter by status</span>
+					<img src="@/assets/icon-arrow-down.svg" alt="" />
+					<ul v-show="filterMenu" class="filter-menu">
+						<li>Draft</li>
+						<li>Pending</li>
+						<li>Paid</li>
+						<li>Clear Filter</li>
+					</ul>
+				</div>
+				<div @click="newInvoice" class="button flex">
+					<div class="inner-button flex">
+						<img src="@/assets/icon-plus.svg" alt="" />
+					</div>
+					<span>New Invoice</span>
+				</div>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
-export default {
-  name: "Home",
-  components: {},
-};
+	export default {
+		name: 'Home',
+		components: {},
+	};
 </script>
