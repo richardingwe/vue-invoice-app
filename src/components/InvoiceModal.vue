a07067b1341862ecc19b3ab80df6d716805acf1e
richardingwe <richardingwe5@gmail.com>
2021-10-18 22:35:00 +0100

    added all the data vaues

--- src/components/InvoiceModal.vue
+++ src/components/InvoiceModal.vue
@@ -7,6 +7,33 @@
 <script>
 	export default {
 		name: 'invoiceModal',
+		data() {
+			return {
+				dateOptions: { year: 'numeric', month: 'short', day: 'numeric' },
+				docId: null,
+				loading: null,
+				billerStreetAddress: null,
+				billerCity: null,
+				billerZipCode: null,
+				billerCountry: null,
+				clientName: null,
+				clientEmail: null,
+				clientStreetAddress: null,
+				clientCity: null,
+				clientZipCode: null,
+				clientCountry: null,
+				invoiceDateUnix: null,
+				invoiceDate: null,
+				paymentTerms: null,
+				paymentDueDateUnix: null,
+				paymentDueDate: null,
+				productDescription: null,
+				invoicePending: null,
+				invoiceDraft: null,
+				invoiceItemList: [],
+				invoiceTotal: 0,
+			};
+		},
 	};
 </script>
 
