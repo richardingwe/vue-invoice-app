6d343acd104026b56546c0a86e11b76c43e3fafe
richardingwe <richardingwe5@gmail.com>
2021-10-23 23:54:32 +0100

    created a mutation inside store

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,16 @@
-import { createStore } from 'vuex'
+import { createStore } from 'vuex';
 
 export default createStore({
   state: {
+    invoiceModal: null
   },
   mutations: {
+    TOGGLE_INVOICE(state) {
+      state.invoiceModal = !state.invoiceModal;
+    }
   },
   actions: {
   },
   modules: {
   }
-})
+});
