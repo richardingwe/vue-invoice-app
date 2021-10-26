d14d86079add8a0e0ee3f9916bb3f8a93b80b876
richardingwe <richardingwe5@gmail.com>
2021-10-26 09:11:38 +0100

    added styling to the modal

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -14,4 +14,29 @@
 	export default {};
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+	.modal {
+		z-index: 100;
+		position: fixed;
+		justify-content: center;
+		align-items: center;
+		height: 100vh;
+		width: 100%;
+		.modal-content {
+			border-radius: 20px;
+			padding: 48px 32px;
+			max-width: 450px;
+			background-color: #252945;
+			color: #fff;
+			p {
+				text-align: center;
+			}
+			.actions {
+				margin-top: 24px;
+				button {
+					flex: 1;
+				}
+			}
+		}
+	}
+</style>
