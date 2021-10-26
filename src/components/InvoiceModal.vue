6d6f8b3b9a4b3b42351a1041ac8c1a20b437bdd8
richardingwe <richardingwe5@gmail.com>
2021-10-26 09:21:25 +0100

    created checkClick function in invoiceModal

--- src/components/InvoiceModal.vue
+++ src/components/InvoiceModal.vue
@@ -235,7 +235,13 @@
 			}
 		},
 		methods: {
-			...mapMutations(['TOGGLE_INVOICE']),
+			...mapMutations(['TOGGLE_INVOICE', 'TOGGLE_MODAL']),
+
+			checkClick(e) {
+				if (e.target === this.$refs.invoiceWrap) {
+					this.TOGGLE_MODAL();
+				}
+			},
 
 			closeInvoice() {
 				this.TOGGLE_INVOICE();
