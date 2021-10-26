f047e7a66a3ded2f4c11e0bab1bbebaa49d567ea
richardingwe <richardingwe5@gmail.com>
2021-10-26 09:10:03 +0100

    added modal markup

---
+++ src/components/Modal.vue
@@ -0,0 +1,17 @@
+<template>
+	<div class="modal flex">
+		<div class="modal-content">
+			<p>Are you sure you want to exit? Your changes will not be saved?</p>
+			<div class="actions flex">
+				<button @click="closeModal" class="purple">Return</button>
+				<button @click="closeInvoice" class="red">Close</button>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+	export default {};
+</script>
+
+<style></style>
