0df4ba394f2822b8744299a7a0f60c5a5409527b
richardingwe <richardingwe5@gmail.com>
2021-10-27 09:37:29 +0100

    added GET_INVOICES actions

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,12 @@
 import { createStore } from 'vuex';
+import db from "../firebase/firebaseInit";
 
 export default createStore({
   state: {
+    invoiceData: [],
     invoiceModal: null,
-    modalActive: null
+    modalActive: null,
+    invoicesLoaded: null,
   },
   mutations: {
     TOGGLE_INVOICE(state) {
@@ -11,9 +14,50 @@ export default createStore({
     },
     TOGGLE_MODAL(state) {
       state.modalActive = !state.modalActive;
-    }
+    },
+    SET_INVOICE_DATA(state, payload) {
+      state.invoiceData.push(payload);
+    },
+    INVOICES_LOADED(state) {
+      state.invoicesLoaded = true;
+    },
   },
   actions: {
+    async GET_INVOICES({ commit, state }) {
+      const getData = db.collection("invoices");
+      const results = await getData.get();
+      results.forEach((doc) => {
+        if (!state.invoiceData.some((invoice) => invoice.docId === doc.id)) {
+          const data = {
+            docId: doc.id,
+            invoiceId: doc.data().invoiceId,
+            billerStreetAddress: doc.data().billerStreetAddress,
+            billerCity: doc.data().billerCity,
+            billerZipCode: doc.data().billerZipCode,
+            billerCountry: doc.data().billerCountry,
+            clientName: doc.data().clientName,
+            clientEmail: doc.data().clientEmail,
+            clientStreetAddress: doc.data().clientStreetAddress,
+            clientCity: doc.data().clientCity,
+            clientZipCode: doc.data().clientZipCode,
+            clientCountry: doc.data().clientCountry,
+            invoiceDateUnix: doc.data().invoiceDateUnix,
+            invoiceDate: doc.data().invoiceDate,
+            paymentTerms: doc.data().paymentTerms,
+            paymentDueDateUnix: doc.data().paymentDueDateUnix,
+            paymentDueDate: doc.data().paymentDueDate,
+            productDescription: doc.data().productDescription,
+            invoiceItemList: doc.data().invoiceItemList,
+            invoiceTotal: doc.data().invoiceTotal,
+            invoicePending: doc.data().invoicePending,
+            invoiceDraft: doc.data().invoiceDraft,
+            invoicePaid: doc.data().invoicePaid,
+          };
+          commit("SET_INVOICE_DATA", data);
+        }
+      });
+      commit("INVOICES_LOADED");
+    },
   },
   modules: {
   }
