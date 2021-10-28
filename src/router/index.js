d64c2a0d2e655bd8dd48cbfc8e6fb4c07f8b6386
richardingwe <richardingwe5@gmail.com>
2021-10-28 22:00:53 +0100

    added the invoice ID route.

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "../views/Home.vue";
+import InvoiceView from "../views/InvoiceView.vue";
 
 const routes = [
   {
     path: "/",
     name: "Home",
     component: Home,
+  },
+  {
+    path: "/invoice/:invoiceId",
+    name: "Invoice",
+    component: InvoiceView,
   },
 ];
 
