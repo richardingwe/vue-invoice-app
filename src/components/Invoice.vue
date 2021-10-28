5e34f27e4c14a3b71035fcab201dd82f09fda5e1
richardingwe <richardingwe5@gmail.com>
2021-10-28 21:36:13 +0100

    added styling

--- src/components/Invoice.vue
+++ src/components/Invoice.vue
@@ -35,3 +35,41 @@
 		props: ['invoice'],
 	};
 </script>
+
+<style lang="scss" scoped>
+	.invoice {
+		text-decoration: none;
+		cursor: pointer;
+		gap: 16px;
+		margin-bottom: 16px;
+		color: #fff;
+		border-radius: 20px;
+		padding: 28px 32px;
+		background-color: #1e2139;
+		align-items: center;
+		span {
+			font-size: 13px;
+		}
+		.left {
+			align-items: center;
+			flex-basis: 60%;
+			gap: 16px;
+			span {
+				flex: 1;
+			}
+			.tracking-number {
+				text-transform: uppercase;
+			}
+		}
+		.right {
+			gap: 16px;
+			flex-basis: 40%;
+			align-items: center;
+			.price {
+				flex: 1;
+				font-size: 16px;
+				font-weight: 600px;
+			}
+		}
+	}
+</style>
