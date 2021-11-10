6569cb61102075ced182788adde634a3289401da
richardingwe <richardingwe5@gmail.com>
2021-11-10 23:55:27 +0100

    created a set_current_invoice mutation

--- src/store/index.js
+++ src/store/index.js
@@ -7,6 +7,7 @@ export default createStore({
     invoiceModal: null,
     modalActive: null,
     invoicesLoaded: null,
+    currentInvoiceArray: null
   },
   mutations: {
     TOGGLE_INVOICE(state) {
@@ -20,6 +21,11 @@ export default createStore({
     },
     INVOICES_LOADED(state) {
       state.invoicesLoaded = true;
+    },
+    SET_CURRENT_INVOICE(state, payload) {
+      state.currentInvoiceArray = state.invoiceData.filter((invoice) => {
+        return invoice.invoiceId === payload;
+      });
     },
   },
   actions: {
