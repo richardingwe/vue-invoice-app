c9056f0d38c599fcb30ddfa72506f39e403c9a0a
richardingwe <richardingwe5@gmail.com>
2021-11-12 23:00:06 +0100

    added status pending

--- src/views/InvoiceView.vue
+++ src/views/InvoiceView.vue
@@ -1,5 +1,26 @@
 <template>
-	<div>{{ currentInvoice.invoiceId }}</div>
+	<div v-if="currentInvoice" class="invoice-view container">
+		<router-link class="nav-link flex" :to="{ name: 'Home' }">
+			<img src="@/assets/icon-arrow-left.svg" alt="" /> Go Back
+		</router-link>
+		<div class="header flex">
+			<div class="left flex">
+				<span>Status</span>
+				<div
+					class="status-button flex"
+					:class="{
+						paid: currentInvoice.invoicePaid,
+						draft: currentInvoice.invoiceDraft,
+						pending: currentInvoice.invoicePending,
+					}"
+				>
+					<span v-if="currentInvoice.invoicePaid">Paid</span>
+					<span v-if="currentInvoice.invoiceDraft">Draft</span>
+					<span v-if="currentInvoice.invoicePending">Pending</span>
+				</div>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
