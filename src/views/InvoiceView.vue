9d7e6098a62c6983808cb18cbc1834189227a1fa
richardingwe <richardingwe5@gmail.com>
2021-11-12 23:05:04 +0100

    added invoice details

--- src/views/InvoiceView.vue
+++ src/views/InvoiceView.vue
@@ -3,6 +3,7 @@
 		<router-link class="nav-link flex" :to="{ name: 'Home' }">
 			<img src="@/assets/icon-arrow-left.svg" alt="" /> Go Back
 		</router-link>
+		<!-- Header -->
 		<div class="header flex">
 			<div class="left flex">
 				<span>Status</span>
@@ -17,6 +18,89 @@
 					<span v-if="currentInvoice.invoicePaid">Paid</span>
 					<span v-if="currentInvoice.invoiceDraft">Draft</span>
 					<span v-if="currentInvoice.invoicePending">Pending</span>
+				</div>
+			</div>
+			<div class="right flex">
+				<button @click="toggleEditInvoice" class="dark-purple">Edit</button>
+				<button @click="deleteInvoice(currentInvoice.docId)" class="red">
+					Delete
+				</button>
+				<button
+					@click="updateStatusToPaid(currentInvoice.docId)"
+					v-if="currentInvoice.invoicePending"
+					class="green"
+				>
+					Mark as Paid
+				</button>
+				<button
+					v-if="currentInvoice.invoiceDraft || currentInvoice.invoicePaid"
+					@click="updateStatusToPending(currentInvoice.docId)"
+					class="orange"
+				>
+					Mark as Pending
+				</button>
+			</div>
+		</div>
+		<!-- Invoice Details -->
+		<div class="invoice-details flex flex-column">
+			<div class="top flex">
+				<div class="left flex flex-column">
+					<p><span>#</span>{{ currentInvoice.invoiceId }}</p>
+					<p>{{ currentInvoice.productDescription }}</p>
+				</div>
+				<div class="right flex flex-column">
+					<p>{{ currentInvoice.billerStreetAddress }}</p>
+					<p>{{ currentInvoice.billerCity }}</p>
+					<p>{{ currentInvoice.billerZipCode }}</p>
+					<p>{{ currentInvoice.billerCountry }}</p>
+				</div>
+			</div>
+			<div class="middle flex">
+				<div class="payment flex flex-column">
+					<h4>Invoice Date</h4>
+					<p>
+						{{ currentInvoice.invoiceDate }}
+					</p>
+					<h4>Payment Date</h4>
+					<p>
+						{{ currentInvoice.paymentDueDate }}
+					</p>
+				</div>
+				<div class="bill flex flex-column">
+					<h4>Bill To</h4>
+					<p>{{ currentInvoice.clientName }}</p>
+					<p>{{ currentInvoice.clientStreetAddress }}</p>
+					<p>{{ currentInvoice.clientCity }}</p>
+					<p>{{ currentInvoice.clientZipCode }}</p>
+					<p>{{ currentInvoice.clientCountry }}</p>
+				</div>
+				<div class="send-to flex flex-column">
+					<h4>Sent To</h4>
+					<p>{{ currentInvoice.clientEmail }}</p>
+				</div>
+			</div>
+			<div class="bottom flex flex-column">
+				<div class="billing-items">
+					<div class="heading flex">
+						<p>Item Name</p>
+						<p>QTY</p>
+						<p>Price</p>
+						<p>Total</p>
+					</div>
+					<div
+						v-for="(item, index) in currentInvoice.invoiceItemList"
+						:key="index"
+						class="item flex"
+					>
+						<p>{{ item.itemName }}</p>
+						<p>{{ item.qty }}</p>
+						<p>{{ item.price }}</p>
+						<p>{{ item.total }}</p>
+					</div>
+				</div>
+				<div class="total flex">
+					<p>Amount Due</p>
+					<p>{{ currentInvoice.invoiceTotal }}</p>
 				</div>
 			</div>
 		</div>
