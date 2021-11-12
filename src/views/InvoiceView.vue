084ab39c676d3240d63dfa9f13dc93c266512478
richardingwe <richardingwe5@gmail.com>
2021-11-12 23:12:10 +0100

    .

--- src/views/InvoiceView.vue
+++ src/views/InvoiceView.vue
@@ -108,7 +108,7 @@
 </template>
 
 <script>
-	import { mapMutations, mapState } from 'vuex';
+	import { mapActions, mapMutations, mapState } from 'vuex';
 	export default {
 		name: 'invoiceView',
 		data() {
@@ -120,15 +120,44 @@
 			this.getCurrentInvoice();
 		},
 		methods: {
-			...mapMutations(['SET_CURRENT_INVOICE']),
-
+			...mapMutations([
+				'SET_CURRENT_INVOICE',
+				'TOGGLE_EDIT_INVOICE',
+				'TOGGLE_INVOICE',
+			]),
+			...mapActions([
+				'DELETE_INVOICE',
+				'UPDATE_STATUS_TO_PENDING',
+				'UPDATE_STATUS_TO_PAID',
+			]),
 			getCurrentInvoice() {
 				this.SET_CURRENT_INVOICE(this.$route.params.invoiceId);
 				this.currentInvoice = this.currentInvoiceArray[0];
 			},
+			toggleEditInvoice() {
+				this.TOGGLE_EDIT_INVOICE();
+				this.TOGGLE_INVOICE();
+			},
+			async deleteInvoice(docId) {
+				await this.DELETE_INVOICE(docId);
+				this.$router.push({ name: 'Home' });
+			},
+			updateStatusToPaid(docId) {
+				this.UPDATE_STATUS_TO_PAID(docId);
+			},
+			updateStatusToPending(docId) {
+				this.UPDATE_STATUS_TO_PENDING(docId);
+			},
 		},
 		computed: {
 			...mapState(['currentInvoiceArray', 'editInvoice']),
+		},
+		watch: {
+			editInvoice() {
+				if (!this.editInvoice) {
+					this.currentInvoice = this.currentInvoiceArray[0];
+				}
+			},
 		},
 	};
 </script>
